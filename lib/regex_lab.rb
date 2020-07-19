7dd04d9c4c5c4942f044c0561f1c54e01e06c705
Jack Goode <jack.w.goode@icloud.com>
2020-07-19 23:41:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,15 @@
 def starts_with_a_vowel?(word)
-
+  array.scan(/\b[aeiou][a-z]*\b/)
+    
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  array = text.split(" ")
+  array.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
